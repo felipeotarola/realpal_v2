94356a32c0863e92e85fe03721ba48f65dcc80e5
felipe otarola <feot1000@gmail.com>
2025-04-22 22:23:47 +0200

    a

--- components/chat-drawer.tsx
+++ components/chat-drawer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { Drawer } from "vaul"
 import { MessageSquare, X } from 'lucide-react'
 import { Button } from "@/components/ui/button"
